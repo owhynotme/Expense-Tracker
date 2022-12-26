04aa6f5128df1745878f98b834267c178b7a5e26
Tarun Kochhar <tarunk999@hotmail.com>
2022-12-26 22:21:10 +0530

    Add Transaction button fully functional

--- lib/UI-Homepage.dart
+++ lib/UI-Homepage.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:expensetracker/Google-Sheets-API.dart';
-import 'package:expensetracker/Plus-Button.dart';
 import 'package:expensetracker/User-card.dart';
 import 'package:expensetracker/pages/loading-page.dart';
 import 'package:flutter/material.dart';
@@ -263,7 +262,7 @@ class _UIhomePageState extends State<UIhomePage> {
                                   ),
 
                                   MaterialButton(
-                                    color: Colors.grey[600],
+                                    color: Colors.grey[400],
                                     child: Text('Enter',
                                         style: TextStyle(color: Colors.white)),
                                     onPressed: () {
